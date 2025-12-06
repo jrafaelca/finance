8a4f47a5f6a90d541c5b2b73932e51d6ac7a7f87
Jose Carrizales <jrafaelca@gmail.com>
2025-12-06 12:42:03 -0300

    refactor(bank): remove unused HasFactory trait

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -2,14 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Bank extends Model
 {
-    /** @use HasFactory<\Database\Factories\BankFactory> */
-    use HasFactory;
-
     /**
      * The attributes that are mass assignable.
      *
